b7efd6c6968a69888a6d6cc1db82c4e83d8b5f22
sunil kumar <iamsunilkumarsk7@gmail.com>
2021-08-02 23:24:10 +0530

    navbar created

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,56 @@
+import logo from './logo.png';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <img className="navbar-brand ps-lg-5" src={logo}></img>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    2/718,Kazura Garen 2nd street...
+                                </a>
+                            </li>
+                        </ul>
+                        <form className="d-md-flex pe-lg-5 me-lg-5">
+                            <div className='d-flex search'>
+                                <input className="form-control me-2 " type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-primary" type="submit">Search</button>                            </div>
+                            <div>
+                                <a className='text-decoration-none login text-body m-3 btn ' href='#' >Login</a>
+                            </div>
+                            <div>
+                                <a className='text-decoration-none text-body m-3 btn ' href='#' >Sign Up</a>
+                            </div>
+                            <div>
+                                <a className='text-decoration-none text-body m-3 btn ' href='#' >Cart</a>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+            <nav className="navbar d-none d-md-block navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand">Navbar</a>
+                    <form className="d-flex  pe-lg-5 me-lg-5">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+
+
+        </div>
+
+
+    );
+}
+export default Navbar;
